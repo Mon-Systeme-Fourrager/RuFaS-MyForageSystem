6784d856ec66ff943327afb205dd1373997691c9
Kass Chupongstimun <k_724@live.com>
2018-01-25 22:16:31 -0600

    Removed dummies and added soil.py

---
+++ MASM/routines/soil.py
@@ -0,0 +1,14 @@
+################################################################################
+#
+# MASM: Modular Agricultural Systems Modeling Environment
+#
+# soil.py - 
+#
+# Authors: Kass Chupongstimun
+#          Jit Patil
+#
+################################################################################
+
+def daily_soil_routine():
+    pass
+ 
