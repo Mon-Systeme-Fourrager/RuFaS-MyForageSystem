41ac5d098c1a145a19063d82397d300bc2914708
Rami ALBASHA <ralbasha@msfourrager.com>
2025-09-19 16:48:13 -0400

    write pydantic models per component schema

--- rufas_api/preprocessing_pydantic_models.py
+++ rufas_api/preprocessing_pydantic_models.py
@@ -1,13 +1,31 @@
+from json import load as load_json
+from pathlib import Path
+from typing import Generator
+
+from yaml import safe_load as load_yaml
+
+
+def get_schemas(path_dir_schemas: Path) -> Generator[dict, None, None]:
+    for p in path_dir_schemas.iterdir():
+        with p.open(mode='r') as f:
+            if (ext := p.suffix) == '.json':
+                yield load_json(f)
+            elif ext in ('.yaml', 'yml'):
+                yield load_yaml(f)
+
+
 if __name__ == "__main__":
     from rufas_api.config import Paths
     from datamodel_code_generator import InputFileType, generate, DataModelType
 
-    generate(
-        input_=Paths.generated_default_properties_specs,
-        input_file_type=InputFileType.OpenAPI,
-        output=Paths.generated_pydantic_models,
-        output_model_type=DataModelType.PydanticV2BaseModel,
-        field_constraints=True,
-        no_alias=True,
-        use_default_kwarg=True
-    )
+    paths = (v for v in Paths.generated_files.iterdir() if v.suffix in ('.json', 'yaml', 'yml'))
+    for pth in paths:
+        generate(
+            input_=pth,
+            input_file_type=InputFileType.JsonSchema if pth.suffix == '.json' else InputFileType.Yaml,
+            output=Paths.generated_files / f"{pth.stem.replace('schema_', 'model_')}.py",
+            output_model_type=DataModelType.PydanticV2BaseModel,
+            field_constraints=True,
+            no_alias=True,
+            use_default_kwarg=True
+        )
