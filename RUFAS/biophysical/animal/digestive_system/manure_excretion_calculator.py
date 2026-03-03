cc15560c05798ccfd6d832648824ffa14fb17249
Haowen Hu <hh598@cornell.edu>
2026-03-02 20:20:38 -0500

    Temporary commit for stress test
    I updated for dry cows too

--- RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
+++ RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
@@ -622,8 +622,8 @@ class ManureExcretionCalculator:
         # TODO: Add TypedDicts for ration_formulation and available feeds - GitHub Issue #1218
         # TODO: Pass in available feeds directly instead of a Feed object - GitHub Issue #1218
         # TODO: Rename abbreviated key names to full names - GitHub Issue #1218
-        dry_matter_intake_original = nutrient_amounts.dry_matter
-        dry_matter_intake = dry_matter_intake_original
+        dry_matter_intake = nutrient_amounts.dry_matter * 0.2
+        dry_matter_intake_original = dry_matter_intake 
         crude_protein_concentration = nutrient_amounts.crude_protein_percentage
         potassium_concentration = nutrient_amounts.potassium_percentage
         ash_concentration = nutrient_amounts.ash_percentage
