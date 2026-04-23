3873409a9dd8d83e77e6afb1272375116a291a3f
Niko <70217952+ew3361zh@users.noreply.github.com>
2026-04-23 16:47:32 -0400

    fixes all broken unit tests

--- tests/test_biophysical/test_manure/test_manure_manager/test_manure_manager.py
+++ tests/test_biophysical/test_manure/test_manure_manager/test_manure_manager.py
@@ -894,12 +894,10 @@ def test_generate_origin_key_invalid_logs_and_raises(manure_manager: ManureManag
     )
 
 
-@pytest.mark.parametrize("animals_simulated", [True, False])
 @pytest.mark.parametrize("use_supplemental_manure", [True, False])
 @pytest.mark.parametrize("request_result_is_none", [True, False])
 def test_request_nutrients(
     mocker: MockerFixture,
-    animals_simulated: bool,
     use_supplemental_manure: bool,
     request_result_is_none: bool,
 ) -> None:
@@ -936,28 +934,23 @@ def test_request_nutrients(
     mocker.patch.object(manure_manager, "_calculate_supplemental_manure_needed", return_value="calculated_supplemental")
 
     # Act
-    actual_results = manure_manager.request_nutrients(mock_nutrient_request, animals_simulated, mock_time)
+    actual_results = manure_manager.request_nutrients(mock_nutrient_request, mock_time)
 
     # Assert
-    if animals_simulated:
-        if request_result_is_none:
-            if use_supplemental_manure:
-                mock_field_request.assert_called_once_with("calculated_supplemental")
-                mock_remove.assert_not_called()
-                assert actual_results == supplemental_result
-            else:
-                assert actual_results is None
+    if request_result_is_none:
+        if use_supplemental_manure:
+            mock_field_request.assert_called_once_with("calculated_supplemental")
+            mock_remove.assert_not_called()
+            assert actual_results == supplemental_result
         else:
-            mock_remove.assert_called_once()
-            if use_supplemental_manure:
-                mock_add_log.assert_called_once()
-                assert actual_results == request_result + supplemental_result
-            else:
-                assert actual_results == request_result
-
+            assert actual_results is None
     else:
-        mock_field_request.assert_called_once_with(mock_nutrient_request)
-        assert actual_results == supplemental_result
+        mock_remove.assert_called_once()
+        if use_supplemental_manure:
+            mock_add_log.assert_called_once()
+            assert actual_results == request_result + supplemental_result
+        else:
+            assert actual_results == request_result
 
 
 @pytest.mark.parametrize("is_nitrogen_limiting_nutrient", [True, False])
